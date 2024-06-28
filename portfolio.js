280c3d8075e2329a2e92d78fc8af80c4dba2c2b6
shubham <152521774+Shubh1127@users.noreply.github.com>
2024-06-28 22:55:51 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+// window.addEventListener('scroll', function () {
+//     const navbar = document.getElementById('navbar');
+//     if (window.scrollY > 50) { // Adjust the scroll value as needed
+//         navbar.classList.add('mirrored');
+//     } else {
+//         navbar.classList.remove('mirrored');
+//     }
+// });
